4e051a413b4d1d35ace4f1415224c9d2be2b8bda
Lionad <tangnad@qq.com>
2020-07-28 17:26:15 +0800

    stash: 从页面找到专辑标题即歌曲信息

--- src/background/index.js
+++ src/background/index.js
@@ -7,7 +7,16 @@ chrome.runtime.onInstalled.addListener(() => {
   console.clear()
   console.log('Installed @', new Date())
 
-  // console.log(api.search('海阔天空'))
-
+  // 注册右键搜索菜单
   registerContextMenus()
+
+  // 注册网易云服务
+  chrome.runtime.onMessage.addListener((msg, _, sendResponse) => {
+    if (msg.api) {
+      const apiType = msg.api
+      const query = msg.query
+      api[apiType] && api[apiType](query).then(sendResponse)
+      return !!api[apiType]
+    }
+  })
 })
--- src/contentScripts/index.js
+++ src/contentScripts/index.js
@@ -1,4 +1,72 @@
 import './index.styl'
 
-// 给专辑页面的歌曲名称前加一个链接到网易云的播放按钮
-// TODO
+/* find album title */
+
+const $albumHeader = document.querySelector('#firstHeading')
+const album = $albumHeader && $albumHeader.innerText
+
+/* find songs info */
+
+const $albumTable = document.querySelector('.wikitable.musicTable')
+const $trs = $albumTable && ($albumTable.querySelectorAll('tr') || [])
+const songs = []
+
+;[...$trs].reduce((h, tr, i) => {
+  const $name =
+    tr.querySelector('.infoRD + .title') || tr.querySelector('.infoYD + .title')
+
+  if ($name && Object.keys(h).length) {
+    songs.push({ ...h })
+    h = Object.create(null)
+  }
+
+  const $label = tr.querySelector('.label')
+  const $text = tr.querySelector('.text')
+
+  if ($name) {
+    const name = $name.innerText
+    h.name = name
+
+    return h
+  } else if ($label && $text) {
+    const label = $label.innerText
+    const text = $text.innerText
+    h[label] = text
+    return h
+  } else {
+    if (i === $trs.length - 1) {
+      songs.push({ ...h })
+    }
+    return h
+  }
+}, Object.create(null))
+
+console.log(album, songs)
+
+/* search album on 163 */
+
+// if (album && songs.length) {
+//     chrome.runtime.sendMessage({
+//       api: 'search',
+//       query: {
+//         keywords: album,
+//         type: 10
+//       }
+//     }, response => {
+//       const { status, result = {} } = response
+//       const { songs = [] } = result
+//       if (status === 200) {
+//       }
+//     })
+// }
+
+// songs.map((songTitle, i) => {
+//   console.log(songTitle)
+//   if (songTitle !== '狂气之瞳　～ Invisible Full Moon') return
+//   chrome.runtime.sendMessage({ api: 'search', query: songTitle }, response => {
+//     const { status, result = {} } = response
+//     const { songs = [] } = result
+//     if (status === 200) {
+//     }
+//   })
+// })
